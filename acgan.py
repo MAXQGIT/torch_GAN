08f024488a14428d630dd19f9ed7d21c0b66dd8d
MAXQGIT <83851714+MAXQGIT@users.noreply.github.com>
2024-04-23 14:27:23 +0800

    Add files via upload

---
+++ acgan.py
@@ -0,0 +1,210 @@
+import argparse
+import os
+import numpy as np
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+
+import torch
+
+os.makedirs("images", exist_ok=True)
+
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02).to(device)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02).to(device)
+        torch.nn.init.constant_(m.bias.data, 0.0).to(device)
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.label_emb = nn.Embedding(10, 100)
+
+        self.init_size = 32 // 4  # Initial size before upsampling
+        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 1, 3, stride=1, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, noise, labels):
+        gen_input = torch.mul(self.label_emb(labels), noise)
+        out = self.l1(gen_input)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            """Returns layers of each discriminator block"""
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.conv_blocks = nn.Sequential(
+            *discriminator_block(1, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        ds_size = 32 // 2 ** 4
+
+        # Output layers
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+        self.aux_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2,10), nn.Softmax(dim=1)) ###nn.Softmax(dim=1)需要注意一下，这是为了让代码不出警告这么改的
+
+    def forward(self, img):
+        out = self.conv_blocks(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+        label = self.aux_layer(out)
+
+        return validity, label
+
+
+# Loss functions
+adversarial_loss = torch.nn.BCELoss()
+auxiliary_loss = torch.nn.CrossEntropyLoss()
+
+# Initialize generator and discriminator
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+
+# Configure data loader
+os.makedirs("../../data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(32), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=64,
+    shuffle=True,
+)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5,0.999))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5,0.999))
+
+
+def sample_image(n_row, batches_done):
+    """Saves a grid of generated digits ranging from 0 to n_classes"""
+    # Sample noise
+    z = torch.FloatTensor(np.random.normal(0, 1, (n_row ** 2, 100))).to(device)
+    # Get labels ranging from 0 to n_classes for n rows
+    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
+    labels = torch.LongTensor(labels).to(device)
+    gen_imgs = generator(z, labels)
+    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+
+
+# ----------
+#  Training
+# ----------
+
+for epoch in range(50):
+    for i, (imgs, labels) in enumerate(dataloader):
+
+        batch_size = imgs.shape[0]
+
+        # Adversarial ground truths
+        valid = torch.ones(batch_size, 1, requires_grad=False).to(device)
+        fake = torch.zeros(batch_size, 1, requires_grad=False).to(device)
+
+        # Configure input
+        real_imgs = torch.FloatTensor(imgs).to(device)
+        labels =torch.LongTensor(labels).to(device)
+
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise and labels as generator input
+        z = torch.FloatTensor(np.random.normal(0, 1, (batch_size, 100))).to(device)
+        gen_labels =torch.LongTensor(np.random.randint(0, 10, batch_size)).to(device)
+
+
+        # Generate a batch of images
+        gen_imgs = generator(z, gen_labels)
+
+        # Loss measures generator's ability to fool the discriminator
+        validity, pred_label = discriminator(gen_imgs)
+        g_loss = 0.5 * (adversarial_loss(validity, valid) + auxiliary_loss(pred_label, gen_labels))
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Loss for real images
+        real_pred, real_aux = discriminator(real_imgs)
+        d_real_loss = (adversarial_loss(real_pred, valid) + auxiliary_loss(real_aux, labels)) / 2
+
+        # Loss for fake images
+        fake_pred, fake_aux = discriminator(gen_imgs.detach())
+        d_fake_loss = (adversarial_loss(fake_pred, fake) + auxiliary_loss(fake_aux, gen_labels)) / 2
+
+        # Total discriminator loss
+        d_loss = (d_real_loss + d_fake_loss) / 2
+
+        # Calculate discriminator accuracy
+        pred = np.concatenate([real_aux.data.cpu().numpy(), fake_aux.data.cpu().numpy()], axis=0)
+        gt = np.concatenate([labels.data.cpu().numpy(), gen_labels.data.cpu().numpy()], axis=0)
+        d_acc = np.mean(np.argmax(pred, axis=1) == gt)
+        d_loss.backward()
+        optimizer_D.step()
+        torch.save(generator,'generator.pt')
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %d%%] [G loss: %f]"
+            % (epoch, 50, i, len(dataloader), d_loss.item(), 100 * d_acc, g_loss.item())
+        )
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % 400 == 0:
+            sample_image(n_row=10, batches_done=batches_done)
